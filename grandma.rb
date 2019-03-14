8ca2648f3f3e3b26d21edf9d873fc0725fc564fc
AJ Guiles <ashiguiles@gmail.com>
2019-03-14 03:43:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,18 +3,13 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
      puts "I LOVE YOU TOO PUMPKIN!"
 
-
-# Whatever you say to grandma, she should respond with
-
-
-# unless you shout it (type in all capitals).
 elsif phrase == phrase.upcase 
 puts "NO, NOT SINCE 1938!"
 
 else if phrase != phrase.upcase 
 puts "HUH?! SPEAK UP, SONNY!"
 
-  
+  puts speak_to_grandma(sdfasdf)
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
@@ -24,4 +19,3 @@ puts "HUH?! SPEAK UP, SONNY!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-puts speak_to_grandma(sdfasdf)
