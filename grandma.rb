2e1ca41fe2ee020769337dd9b28058e564b4ceac
AJ Guiles <ashiguiles@gmail.com>
2019-03-14 03:48:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,10 @@ def speak_to_grandma(phrase)
      return "I LOVE YOU TOO PUMPKIN!"
 
 elsif phrase == phrase.upcase 
-return "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
 
 else phrase != phrase.upcase 
-puts "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 end
 end
 
